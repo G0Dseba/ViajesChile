5d39c827ab05d0df565a9080f098454c60d6f670
G0Dseba <sebaxt.k07@gmail.com>
2024-12-12 12:41:52 -0300

    Agregando Cards que se ocultan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Carrousel
 $(document).ready(function () {
     $('#customCarousel').carousel(); 
 
@@ -7,5 +8,16 @@ $(document).ready(function () {
 
     $('.next-btn').on('click', function () {
         $('#customCarousel').carousel('next'); 
+    });
+});
+
+//tooltip con de correo
+document.addEventListener('DOMContentLoaded', function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+    $("#enviarCorreo").click(function () {
+        alert("El formulario fue enviado correctamente...");
     });
 });
